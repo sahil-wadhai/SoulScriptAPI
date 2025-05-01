d6b7e4dbb09b81e9b142c54749a7c7d54f33ce16
Sahil Wadhai <wadhai.sahil101@gmail.com>
2025-05-01 19:19:36 +0530

    add mapping from dto to entity and vice-versa

--- src/main/java/com/dev/soulScript_Api/mapper/JournalEntryMapper.java
+++ src/main/java/com/dev/soulScript_Api/mapper/JournalEntryMapper.java
@@ -1,4 +1,35 @@
 package com.dev.soulScript_Api.mapper;
 
+import com.dev.soulScript_Api.dto.JournalEntryRequestDTO;
+import com.dev.soulScript_Api.dto.JournalEntryResponseDTO;
+import com.dev.soulScript_Api.model.JournalEntry;
+import com.dev.soulScript_Api.model.User;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
 public class JournalEntryMapper {
+
+    // Convert Entity to Response DTO
+    public static JournalEntryResponseDTO toResponse(JournalEntry journal) {
+        return JournalEntryResponseDTO.builder()
+                .id(journal.getId())
+                .title(journal.getTitle())
+                .content(journal.getContent())
+                .createdAt(journal.getCreatedAt())
+                .username(journal.getUser().getUsername())
+                .userFullName(journal.getUser().getFullName())
+                .build();
+    }
+
+    // Convert Request DTO to Entity
+    public static JournalEntry toEntity(JournalEntryRequestDTO dto, User user) {
+        return JournalEntry.builder()
+                .title(dto.getTitle())
+                .content(dto.getContent())
+                .createdAt(LocalDateTime.now()) // You can also use @PrePersist in entity
+                .user(user)
+                .build();
+    }
 }
--- src/main/java/com/dev/soulScript_Api/mapper/UserMapper.java
+++ src/main/java/com/dev/soulScript_Api/mapper/UserMapper.java
@@ -1,4 +1,26 @@
 package com.dev.soulScript_Api.mapper;
 
+import com.dev.soulScript_Api.dto.UserRequestDTO;
+import com.dev.soulScript_Api.dto.UserResponseDTO;
+import com.dev.soulScript_Api.model.User;
+
 public class UserMapper {
+
+    public static User toEntity(UserRequestDTO request) {
+        return User.builder()
+                .fullName(request.getFullName())
+                .username(request.getUsername())
+                .password(request.getPassword())
+                .email(request.getEmail())
+                .build();
+    }
+
+    public static UserResponseDTO toResponse(User user) {
+        return UserResponseDTO.builder()
+                .id(user.getId())
+                .fullName(user.getFullName())
+                .username(user.getUsername())
+                .email(user.getEmail())
+                .build();
+    }
 }
